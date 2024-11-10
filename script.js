8d5f8db0d5f650d48997c28ceb24311f68d3a0e1
Deleronvi <byronochieng467@gmail.com>
2024-11-10 16:10:06 +0300

    current operand

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 
-class calculator{
+class Calculator{
     constructor(previousOperandTextElement, currentOperandTextElement){
         this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
-        
+     this.clear()
     }
+
+clear(){
+    this.currentOperand = " "
+     this.previousOperand = ""
+      this.operation = "undefined"
+
+}
+delete(){
+    
+}
+appendNumber(number){
+
+    if (number == '.' && this.currentOperand.includes('.')) return
+    this.currentOperand = this.currentOperand.toString() + number.toString()
+    
+}
+chooseOpertion(operation){
+    
+}
+
+compute(){
+    
+}
+updateDisplay(){
+    this.currentOperandTextElement.innerText = this.currentOperand
+    
+}
+
 
 
 }
@@ -18,3 +46,14 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+
+const calculator = new Calculator(previousOperandTextElement, 
+    currentOperandTextElement)
+
+    numberButtons.forEach(button => {
+        button.addEventListener('click', ()=> {
+            calculator.appendNumber(button.innerText)
+            calculator.updateDisplay()
+        })
+    })
